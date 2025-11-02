7cb4357189cf797a6087bd8c542b628a54d702d5
kaarthika1409 <154883233+kaarthika1409@users.noreply.github.com>
2025-11-02 19:13:08 +0530

    UPDATED ALTAS VARIABLE

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ require('./models');
 
 // ✅ MongoDB Connection using Atlas URI from environment variable
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
